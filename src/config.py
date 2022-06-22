20dc8eb3bbc609d114010414cfc137982d77d0d2
Akshay Bharadwaj <akshay.bharadwaj@ucalgary.ca>
2022-06-21 21:13:51 -0600

    updated Config file

--- src/config.py
+++ src/config.py
@@ -4,21 +4,22 @@ image = dict(height = 375,
              width = 1242,
              channel = 3) 
 
-trainingDataFile = "/home/akshay/pytorchEnv/targetless_calibration/parsed_set_10_5.txt"
+trainingDataFile = "/home/akshay/pytorchEnv/targetless_calibration/parsed_set_20_10.txt"
 maxPtCldSizeFile = "/home/akshay/pytorchEnv/targetless_calibration/maxPtCldSize.txt"
 camToCamCalibFile = "/mnt/291d3084-ca91-4f28-8f33-ed0b64be0a8c/akshay/kitti/raw/2011_09_26/calib_cam_to_cam.txt"
 camToVeloCalibFile = "/mnt/291d3084-ca91-4f28-8f33-ed0b64be0a8c/akshay/kitti/raw/2011_09_26/calib_velo_to_cam.txt"
 
 pathToPretrainedModel = "/home/akshay/pytorchEnv/targetless_calibration/src/model/trained/bestTargetCalibrationModel.pth"
+pathToSWINPreTrainedModel = "/home/akshay/pytorchEnv/targetless_calibration/src/model/trained/SWINPath.pth"
 
 logsDirs = "/home/akshay/pytorchEnv/targetless_calibration/logs/"
 
 calibrationDir = "/mnt/data/akshay/kitti/raw/2011_09_26/"
 
 training = dict(
-    batchSize = 12,
+    batchSize = 8,
     epoch = 35,
-    learningRate = 0.0003,
+    learningRate = 0.00003,
     beta0 = 0.9,
     beta1 = 0.999,
     eps = 1e-08,
